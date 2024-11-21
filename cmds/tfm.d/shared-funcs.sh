cb1f5fe5e0a4aa9adbe3a326991f53fc6154b12e
James Price <jimmyjayp@gmail.com>
2024-11-20 23:22:01 -0500

    Fix record results func

--- cmds/tfm.d/shared-funcs.sh
+++ cmds/tfm.d/shared-funcs.sh
@@ -1,7 +1,9 @@
 
 function tfm_record_results {
 	_echo "Creating OUTPUT.json and STATE.md"
-	_run terraform output -json | jq .props.value >OUTPUT.json
+	_vecho "_run terraform output -json | jq .props.value >OUTPUT.json"
+	terraform output -json | jq .props.value >OUTPUT.json
 	echo -e "# Terraform State List\n" >STATE.md
-	_run terraform state list >>STATE.md
+	_vecho terraform state list >>STATE.md
+	terraform state list >>STATE.md
 }
