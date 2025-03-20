1e4700eab67a3410da783c189985ee583d64e241
James Price <jimmyjayp@gmail.com>
2025-03-20 16:07:09 -0400

    Enable short command aliases by default

--- etc/skel/clamity-home/aliases.sh
+++ etc/skel/clamity-home/aliases.sh
@@ -1,5 +1,5 @@
-# If you want the standard clamity aliases defined for your shell when you load
-# clamity, uncomment this line. Or, you can define your own aliases here.
+# If you don't want the standard clamity aliases defined for your shell when you
+# load clamity, comment out this line. Or, you can define your own aliases here.
 # They'll be loaded when clamity is loaded.
 
-# source $CLAMITY_ROOT/etc/common-aliases.sh
+source $CLAMITY_ROOT/etc/common-aliases.sh
